a92a42b2af82a9bc1db41f8cb6f70a0dfcd86013
Suraj A L <slandage@huronconsultinggroup.com>
2024-07-18 17:03:46 +0530

    20240718

---
+++ Python/PCAP™ – Certified Associate in Python Programming/Chapter 03 - Strings/20240718/operations_on_strings_min.py
@@ -0,0 +1,37 @@
+# Operations on strings: min()
+
+# Demonstrating min() - Example 1:
+print()
+string = "aAbByYzZ"
+print("String: " + string)
+print()
+print("min value in string is -- " + min(string))
+print()
+print("ASCII values of characters in the string:")
+for char in string:
+    ascii_value = ord(char)
+    print(f"ASCII value of '{char}' is: {ascii_value}")
+
+print()
+
+# Demonstrating min() - Examples 2 & 3:
+t = 'The Knights Who Say "Ni!"'
+print("String: " + t)
+print()
+print("min value in string is -- "+'[' + min(t) + ']')
+print()
+for char in t:
+    ascii_value = ord(char)
+    print(f"ASCII value of '{char}' is: {ascii_value}")
+print()
+
+
+t = [0, 1, 2]
+print(f"String: {t}")
+print()
+print(f"min value in string is -- {min(t)}")
+print()
+for char in t:
+    ascii_value = ord(chr(char))
+    print(f"ASCII value of '{char}' is: {ascii_value}")
+print()
